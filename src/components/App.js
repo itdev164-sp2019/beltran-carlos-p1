46eb6c52dd474e17c6c803f33fad95316ccfa846
Carlos Felix-Beltran <felixbec@gmatc.matc.edu>
2019-03-04 10:43:27 -0600

    implemented sass

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        
+          <p>
+          <h1 className='sidebar__title'>Recipe Book</h1>
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
